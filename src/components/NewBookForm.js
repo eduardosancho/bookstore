b36c498a9252f0be58233f2cf208953e375a2dbe
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-02-22 11:24:52 -0600

    Prepare to send form data to redux store

--- src/components/NewBookForm.js
+++ src/components/NewBookForm.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import './NewBookForm.module.css';
+import { v4 as uuidv4 } from 'uuid';
+// import { useDispatch } from 'react-redux';
+// import { addBook, removeBook } from '../redux/books/books';
 // import propTypes from 'prop-types';
+
+// const dispatch = useDispatch();
 
 class NewBookForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
       title: '',
+      author: '',
     };
   }
 
@@ -18,11 +24,22 @@ class NewBookForm extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-    // Insert form validation
+    // this.submitBookToStore(e.target);
+    console.log(e.target.title.value);
+    console.log(e.target.author.value);
   };
 
+  submitBookToStore = (form) => {
+    const newBook = {
+      id: uuidv4(),
+      title: form.title.value,
+      author: form.author.value,
+    };
+    dispatch(addBook(newBook));
+  }
+
   render() {
-    const { title } = this.state;
+    const { title, author } = this.state;
     return (
       <form onSubmit={this.handleSubmit}>
         <p>ADD NEW BOOK</p>
@@ -32,6 +49,14 @@ class NewBookForm extends Component {
           placeholder="Book title"
           value={title}
           name="title"
+          onChange={this.onChange}
+        />
+        <input
+          id="author-field"
+          type="text"
+          placeholder="Author"
+          value={author}
+          name="author"
           onChange={this.onChange}
         />
         <select>
